45ff17f93bb9edf7ed833a0fb274985f92cedac8
KrisRud <krissfd@gmail.com>
2025-03-29 21:00:23 +0700

    Добавлены тесты на сравнение прямоугольников и квадратов

--- sandbox/src/main/java/ru/stqa/geometry/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry;
 
+import java.util.Objects;
+
 public record Rectangle(double a, double b)  {
 
     public Rectangle {
@@ -8,7 +10,20 @@ public record Rectangle(double a, double b)  {
         }
     }
 
-   public void printRectangleArea(double a, double b) {
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(a, rectangle.a) == 0 && Double.compare(b, rectangle.b) == 0
+                || Double.compare(b, rectangle.a) == 0 && Double.compare(a, rectangle.b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+    public void printRectangleArea(double a, double b) {
        var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a, b, rectangleArea(a, b));
        System.out.println(text);
    }

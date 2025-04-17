e38312733d5ce2ce2429febb176255bd3fa41d3f
KrisRud <krissfd@gmail.com>
2025-04-17 14:56:50 +0700

    Добавлена возможность запуска тестов в разных браузерах

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -20,9 +20,22 @@ public class ApplicationManager {
                 options.setBinary("/home/kristina/firefox");
                 driver = new FirefoxDriver(options);
             } else if ("chrome".equals(browser)) {
-                var options = new ChromeOptions();
-                options.setBinary("/opt/google/chrome");
+                //var options = new ChromeOptions();
+                //options.setBinary("/opt/google/chrome");
+                //driver = new ChromeDriver(options);
+                ChromeOptions options = new ChromeOptions();
+
+                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+                //options.addArguments("--headless");
+                //options.addArguments("--no-sandbox");
+                //options.addArguments("start-maximized");
+                //options.addArguments("disable-infobars");
+                //options.addArguments("--disable-extensions");
+
                 driver = new ChromeDriver(options);
+                //driver.implicitly_wait(10);
+
             } else {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
             }
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -2,20 +2,23 @@ package tests;
 
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.Map;
 
 public class TestBase {
 
     protected static ApplicationManager app;
 
-    //JavascriptExecutor js;
-    //Map<String, Object> vars;
+    JavascriptExecutor js;
+    Map<String, Object> vars;
 
     @BeforeEach
     public void setUp() {
         if (app == null) {
             app = new ApplicationManager();
         }
-        app.init(System.getProperty("browser", "firefox"));
+        app.init(System.getProperty("browser", "chrome"));
     }
 
 }

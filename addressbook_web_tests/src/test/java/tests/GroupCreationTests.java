cc950337abff42de06d7266eb43bc635ec75208c
KrisRud <krissfd@gmail.com>
2025-04-19 20:54:57 +0700

    Добавление подсчета количества элементов в коллекции

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import model.GroupData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
@@ -9,7 +10,10 @@ public class GroupCreationTests extends TestBase {
 
     @Test
     public void canCreateGroup() {
+        int groupCount = app.groups().getCount();
         app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount + 1, newGroupCount);
 
     }
 
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -18,7 +18,7 @@ public class TestBase {
         if (app == null) {
             app = new ApplicationManager();
         }
-        app.init(System.getProperty("browser", "chrome"));
+        app.init(System.getProperty("browser", "firefox"));
     }
 
 }

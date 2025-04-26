43de108ad55ac680ebbb6fb37f546e5e897588d0
KrisRud <krissfd@gmail.com>
2025-04-26 11:07:13 +0700

    Добавлен метод создания нескольких групп за раз (тема Цикл со счётчиком)

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -4,7 +4,9 @@ import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.JavascriptExecutor;
 
+import javax.swing.*;
 import java.util.Map;
+import java.util.Random;
 
 public class TestBase {
 
@@ -19,6 +21,15 @@ public class TestBase {
             app = new ApplicationManager();
         }
         app.init(System.getProperty("browser", "firefox"));
+    }
+
+    public String randomString(int n) {
+        var rnd = new Random();
+        var result = "";
+        for (int i = 0; i < n; i++) {
+            result = result + (char)('a' + rnd.nextInt(26));
+        }
+        return result;
     }
 
 }

70454351ec8b9d1a4ae2d11ac907fed71db9f2ac
KrisRud <krissfd@gmail.com>
2025-05-09 20:30:43 +0700

    Object Relational Mapping (ORM)

--- addressbook_web_tests/src/test/java/manager/ApplicationManager.java
+++ addressbook_web_tests/src/test/java/manager/ApplicationManager.java
@@ -16,6 +16,7 @@ public class ApplicationManager {
     private GroupHelper groups;
     private  ContactHelper contacts;
     private  JdbcHelper jdbc;
+    private  HibernateHelper hbm;
     private Properties properties;
 
 
@@ -91,6 +92,13 @@ public class ApplicationManager {
             jdbc = new JdbcHelper(this);
         }
         return  jdbc;
+    }
+
+    public  HibernateHelper hbm () {
+        if (hbm == null) {
+            hbm = new HibernateHelper(this);
+        }
+        return  hbm;
     }
 
 }

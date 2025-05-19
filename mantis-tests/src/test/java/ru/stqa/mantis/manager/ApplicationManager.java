3d89fd89ac64fec3a256afa583bfcec880f6a72b
Kristina <krissfd@gmail.com>
2025-05-19 08:07:39 +0300

    убраны закомментированые строки

--- mantis-tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -20,32 +20,13 @@ public class ApplicationManager {
     public WebDriver driver() {
         if (driver == null) {
             if ("firefox".equals(string)) {
-                //var options = new FirefoxOptions();
-                //options.setBinary(properties.getProperty("local.firefox"));
                 driver = new FirefoxDriver();
             } else if ("chrome".equals(string)) {
-                //var options = new ChromeOptions();
-                //options.setBinary("/opt/google/chrome");
-                //driver = new ChromeDriver(options);
-                //ChromeOptions options = new ChromeOptions();
-
-                //System.setProperty(properties.getProperty("local.chrome"), properties.getProperty("local.chromedriver"));
                 driver = new ChromeDriver();
-                //options.addArguments("--headless");
-                //options.addArguments("--no-sandbox");
-                //options.addArguments("start-maximized");
-                //options.addArguments("disable-infobars");
-                //options.addArguments("--disable-extensions");
-
-                //driver = new ChromeDriver(options);
-                //driver.implicitly_wait(10);
-
             } else {
                 throw new IllegalArgumentException(String.format("Unknown browser %s", string));
             }
 
-            //testBase.js = (JavascriptExecutor) driver;
-            //testBase.vars = new HashMap<String, Object>();
             Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
             driver.get(properties.getProperty("web.baseUrl"));
             driver.manage().window().setSize(new Dimension(1850, 1053));
